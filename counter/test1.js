d33f08e26fe736b1dd6220ce9000d4bcee8d265b
adhnanjeff <145822540+adhnanjeff@users.noreply.github.com>
2024-02-21 23:31:21 +0530

    Add files via upload

---
+++ counter/test1.js
@@ -0,0 +1,18 @@
+let incbtn = document.getElementById("ibtn");
+let decbtn = document.getElementById("dbtn");
+let rsbtn = document.getElementById("rbtn");
+let count = 0;
+
+incbtn.onclick = function() {
+    count++;
+    document.getElementById("myLabel").textContent= count; 
+}
+
+decbtn.onclick = function() {
+    count--;
+    document.getElementById("myLabel").textContent= count; 
+}
+rsbtn.onclick = function () {
+    count = 0;
+    document.getElementById("myLabel").textContent= count; 
+}

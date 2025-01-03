8178ec1034aa9820e28503a0d87102cc1e6a8d29
adhnanjeff <adhnanjeff26@gmail.com>
2025-01-03 23:17:36 +0530

    edit methods

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ function updateClockTime(){
     let hours = now.getHours();
     const meridian = hours >= 12 ? "PM" : "AM";
     
-    hours = hours.toString().padStart(2, 0);
+    hr = hours.toString().padStart(2, 0);
     const min = now.getMinutes().toString().padStart(2, 0);;
     const sec = now.getSeconds().toString().padStart(2, 0);
-    const time = `${hours}:${min}:${sec} ${meridian}`;
+    const time = `${hr}:${min}:${sec} ${meridian}`;
     document.getElementById("clock").textContent = time
 
 }

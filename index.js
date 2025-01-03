3a55c59cf4be20cf0ae69c21903a5c2b63e10db0
adhnanjeff <adhnanjeff26@gmail.com>
2025-01-03 23:11:00 +0530

    edits on function name

---
+++ index.js
@@ -0,0 +1,17 @@
+function updateClockTime(){
+
+    const now = new Date()
+    let hours = now.getHours();
+    const meridian = hours >= 12 ? "PM" : "AM";
+    
+    hours = hours.toString().padStart(2, 0);
+    const min = now.getMinutes().toString().padStart(2, 0);;
+    const sec = now.getSeconds().toString().padStart(2, 0);
+    const time = `${hours}:${min}:${sec} ${meridian}`;
+    document.getElementById("clock").textContent = time
+
+}
+
+
+updateClock()
+setInterval(updateClockTime, 1000)

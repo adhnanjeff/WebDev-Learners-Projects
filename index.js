ebe0fd706b9da0e1667e7462c93436b547d4cc17
adhnanjeff <adhnanjeff26@gmail.com>
2025-01-03 20:12:04 +0530

    updated code

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ function updateClockTime(){
     const meridian = hours >= 12 ? "PM" : "AM";
     
     hr = hours.toString().padStart(2, 0);
-    const min = now.getMinutes().toString().padStart(2, 0);;
-    const sec = now.getSeconds().toString().padStart(2, 0);
-    const time = `${hr}:${min}:${sec} ${meridian}`;
+    const m = now.getMinutes().toString().padStart(2, 0);;
+    const s = now.getSeconds().toString().padStart(2, 0);
+    const time = `${hr}:${m}:${s} ${meridian}`;
     document.getElementById("clock").textContent = time
 
 }
